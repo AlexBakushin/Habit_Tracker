8457a92a5932ff1486a0c2b3ffa020e3a7207aa5
AlexBakushin <bakushin35@gmail.com>
2024-03-22 14:58:51 +0500

    add telegram application services

---
+++ main/services.py
@@ -0,0 +1,24 @@
+from telethon import TelegramClient
+from telethon.tl.functions.users import GetFullUserRequest
+from django.conf import settings
+
+
+api_id = settings.API_ID
+api_hash = settings.API_HASH
+
+
+async def get_user(username):
+    async with TelegramClient('@alex_bakushin', api_id, api_hash) as client:
+        user = await client(GetFullUserRequest(username))
+    return user
+
+
+async def main(username):
+    data = await get_user(username)
+    position = data.stringify().find("id=")
+    user_id_full = data.stringify()[position + 3:position + 13]
+    if user_id_full.endswith(","):
+        user_id = user_id_full[:-1]
+        return user_id
+    else:
+        return user_id_full
--- main/tasks.py
+++ main/tasks.py
@@ -2,6 +2,8 @@ import requests
 from celery import shared_task
 from main.models import Habit
 from django.conf import settings
+import asyncio
+from main.services import main
 
 
 @shared_task
@@ -10,22 +12,16 @@ def send_message(tg_user_name, habit_id):
     Задача для отправки уведомления о выполнении привычки в телеграм
     """
     bot_token = settings.TELEGRAM_BOT_TOKEN
-    chats_info = requests.get(
-        url=f'https://api.telegram.org/bot{bot_token}/getUpdates',
+    chat_id = asyncio.run(main(tg_user_name))
+    habit = Habit.objects.get(habit_id=habit_id)
+    response = requests.get(
+        url=f'https://api.telegram.org/bot{bot_token}/sendMessage',
+        params={
+            'chat_id': chat_id,
+            'text': f'Привет!\n'
+                    f'Пора выполнить свою привычку- {habit.action} {habit.place} в течение {habit.time_to_complete} секунд!'
+        }
     )
-    for item in chats_info.json()['result']:
-        username = item['message']['chat']['username']
-        if username == tg_user_name[1:]:
-            chat_id = item['message']['chat']['id']
-            habit = Habit.objects.get(habit_id=habit_id)
-            response = requests.get(
-                url=f'https://api.telegram.org/bot{bot_token}/sendMessage',
-                params={
-                    'chat_id': chat_id,
-                    'text': f'Привет!\n'
-                            f'Пора выполнить свою привычку- {habit.action} {habit.place} в течение {habit.time_to_complete} секунд!'
-                }
-            )
 
 # celery -A main worker -l info -P eventlet
 # celery -A main beat -l info -S django
